0a2550a898013a64220500155cb95aeff677773c
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-22 21:34:11 +0530

    add dir

---
+++ music-edtech/src/app/admin/page.tsx
@@ -0,0 +1,4 @@
+export default function AdminPage() {
+    return (
+        <><h2 className="bg-zinc-800 border-2 border-zinc-700 text-4xl font-bold text-zinc-200 mt-32 mx-48 text-center rounded-full shadow-md shadow-zinc-500 py-5 px-4">Admin Page</h2></>)
+}
