3a4430b9fc786db9978ed5a50d7a09fec88aa8c3
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-24 22:55:40 +0530

    "Added dark mode and updated HTML and H1 elements with Tailwind CSS classes"

--- music-edtech/src/app/page.tsx
+++ music-edtech/src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl text-center font-bold mt-36">Piyush Kumar Raikwar</h1>
+    <h1 className="text-4xl text-center font-bold mt-36 bg-zinc-900">Piyush Kumar Raikwar</h1>
   );
 }
